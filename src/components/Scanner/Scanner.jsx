de48e3776155bc625b89a98e35e51efd2c147721
Shunji Zhan <shunhahaha@live.com>
2021-09-10 16:04:54 +0800

    poc finished
    - able to async fetch all data and async generate table
    - able to show a progress count

--- src/components/Scanner/Scanner.jsx
+++ src/components/Scanner/Scanner.jsx
@@ -1,10 +1,18 @@
 import React, {
+  useReducer,
   useState,
+  useRef,
 } from 'react';
 
-import { ApiPromise, WsProvider } from '@polkadot/api';
+import { Table } from 'antd';
+
+import {
+  createRpc,
+  getEventsForBlock,
+} from '../../utils/utils';
 
 import '../../styles.scss';
+import 'antd/dist/antd.css';
 
 const InputField = ({ value, onChange }) => (
   <input
@@ -14,47 +22,136 @@ const InputField = ({ value, onChange }) => (
   />
 );
 
+const Progress = ({ cur, all }) => (
+  <div>
+    { `finished: ${cur}/${all}` }
+  </div>
+);
+
+const EventTable = ({ dataSource }) => {
+  const columns = [
+    {
+      title: 'Section',
+      dataIndex: 'section',
+      key: 'section',
+    },
+    {
+      title: 'Method',
+      dataIndex: 'method',
+      key: 'method',
+    },
+    {
+      title: 'Events',
+      dataIndex: 'events',
+      key: 'events',
+    },
+  ];
+
+  return (
+    <Table
+      dataSource={ dataSource }
+      columns={ columns }
+    />
+  );
+};
+
 const Scanner = () => {
-  const [startBlock, setStartBlock] = useState(1);
-  const [endBlock, setEndBlock] = useState(100);
+  const [startBlock, setStartBlock] = useState(6763530);
+  const [endBlock, setEndBlock] = useState(6763540);
   const [rpc, setRpc] = useState('wss://rpc.polkadot.io');
+  const [events, setEvents] = useState([]);
+  const [count, setCount] = useState(-1);
+  const [isInitializing, setIsInitializing] = useState(true);
+  const prevApi = useRef();
+
+  const totalBlocks = endBlock - startBlock + 1;
+
+  useState(() => {
+    (async () => {
+      const api = await createRpc(rpc);
+      prevApi.current = { api, rpc };
+      setIsInitializing(false);
+    })();
+  }, []);
 
   const handleStartBlockChange = e => setStartBlock(e.target.value);
   const handleEndBlockChange = e => setEndBlock(e.target.value);
   const handleRpcChange = e => setRpc(e.target.value);
 
-  const fetchData = async () => {
-    console.log(`connecting using ${rpc}...`);
-    const wsProvider = new WsProvider(rpc);
-    const api = await ApiPromise.create({ provider: wsProvider });
-    console.log('connected!!');
+  const getApi = async () => {
+    if (rpc === prevApi.current.rpc) {
+      return prevApi.current.api;
+    }
 
-    console.log(api.genesisHash.toHex());
-  }
+    const api = await createRpc(rpc);
+    prevApi.current = { api, rpc };
+
+    return api;
+  };
+
+  const fetchData = async () => {
+    setEvents([]);
+    setCount(0);
+
+    const api = await getApi();
+
+    const _fetch = async block => {
+      console.log(`fethcing ${block}`);
+      const data = await getEventsForBlock(api, block);
+      setEvents(e => [...e, ...data]);
+      setCount(c => c + 1);
+
+      console.log('data:', data);
+      return data;
+    };
+
+    const pendings = [];
+    for (let block = startBlock; block <= endBlock; block++) {
+      pendings.push(_fetch(block));
+    }
+
+    await Promise.all(pendings);
+  };
 
   return (
     <div id='Scanner'>
-      <section id='input-container'>
-        <InputField
-          value={ startBlock }
-          onChange={ handleStartBlockChange }
-        />
-        <InputField
-          value={ endBlock }
-          onChange={ handleEndBlockChange }
-        />
-        <InputField
-          value={ rpc }
-          onChange={ handleRpcChange }
-        />
-        <button
-          id='input-container__button'
-          type='button'
-          onClick={ fetchData }
-        >
-          fetch data
-        </button>
-      </section>
+      {isInitializing && <span style={{ fontSize: '200%' }}>connecting to { rpc } ...</span> }
+
+      { !isInitializing && (
+        <div>
+          <section id='input-container'>
+            <InputField
+              value={ startBlock }
+              onChange={ handleStartBlockChange }
+            />
+            <InputField
+              value={ endBlock }
+              onChange={ handleEndBlockChange }
+            />
+            <InputField
+              value={ rpc }
+              onChange={ handleRpcChange }
+            />
+
+            <button
+              id='input-container__button'
+              type='button'
+              onClick={ fetchData }
+            >
+              fetch data
+            </button>
+          </section>
+          { count > -1
+            && (
+              <Progress
+                cur={ count }
+                all={ totalBlocks }
+              />
+            )}
+
+          { !!events.length && <EventTable dataSource={ events } /> }
+        </div>
+      )}
     </div>
   );
 };
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,2 +1,42 @@
-export const plus = x => x + 1;
+import { ApiPromise, WsProvider } from '@polkadot/api';
 
+export const createRpc = async endpoint => {
+  console.log(`connecting using ${endpoint}...`);
+
+  const wsProvider = new WsProvider(endpoint);
+  const api = await ApiPromise.create({ provider: wsProvider });
+
+  console.log('connected!!');
+
+  return api;
+};
+
+export const getSignedBlock = async (api, blockNumber) => {
+  const blockHash = await api.rpc.chain.getBlockHash(blockNumber);
+  const signedBlock = await api.rpc.chain.getBlock(blockHash);
+
+  return signedBlock;
+};
+
+export const getEventsForBlock = async (api, blockNumber) => {
+  const signedBlock = await getSignedBlock(api, blockNumber);
+  const allRecords = await api.query.system.events.at(signedBlock.block.header.hash);
+
+  const res = [];
+  signedBlock.block.extrinsics.forEach(({ method: { method, section } }, index) => {
+    // filter the specific events based on the phase and then the index of our extrinsic in the block
+    const events = allRecords
+      .filter(({ phase }) => (
+        phase.isApplyExtrinsic
+        && phase.asApplyExtrinsic.eq(index)))
+      .map(({ event }) => `${event.section}.${event.method}`);
+
+    res.push({
+      section,
+      method,
+      events: events.join(', ') || 'no events',
+    });
+  });
+
+  return res;
+};
